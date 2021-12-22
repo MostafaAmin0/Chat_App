c82d496af85d586e55b4206e4fe0f24752fa7b08
MostafaAmin0 <mostafa.aminmk@gmail.com>
2021-12-22 02:02:56 +0200

    read data from firebase

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({Key? key}) : super(key: key);
@@ -6,10 +7,18 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (ctx, index) {
-          return const Center();
+      body: const Center(child: Text("loading")),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          FirebaseFirestore.instance
+              .collection('/Chat/qeWt7Q0aw6JUc0s7OnTb/message')
+              .snapshots()
+              .listen((event) {
+              event.docs.forEach((element) { 
+                print(element['text']);
+              });
+          });
         },
       ),
     );
