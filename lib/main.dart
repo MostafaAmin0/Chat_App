b3252bef2f393d461fcdfa718edb371535cc16bc
MostafaAmin0 <mostafa.aminmk@gmail.com>
2022-01-05 22:48:11 +0200

    authScreen with simple form

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 import '../screens/chat_screen.dart';
+import '../screens/auth_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const ChatScreen(),
+      home: const AuthScreen(),
     );
   }
 }
