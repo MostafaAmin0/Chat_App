35a0c6c7cfc28022eee9d6149fa6c6dea7e50478
MostafaAmin0 <mostafa.aminmk@gmail.com>
2022-02-14 18:45:58 +0200

    send message text field

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,7 +1,8 @@
-import 'package:chat_app/widgets/chat/messages.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+
+import '../widgets/chat/messages.dart';
+import '../widgets/chat/new_message.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({Key? key}) : super(key: key);
@@ -47,18 +48,9 @@ class ChatScreen extends StatelessWidget {
             Expanded(
               child: Messages(),
             ),
+            NewMessage(),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: () {
-          FirebaseFirestore.instance
-              .collection('/Chat/qeWt7Q0aw6JUc0s7OnTb/message')
-              .add({
-            'text': 'Button pressed',
-          });
-        },
       ),
     );
   }
---
+++ lib/widgets/chat/new_message.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class NewMessage extends StatefulWidget {
+  const NewMessage({Key? key}) : super(key: key);
+
+  @override
+  _NewMessageState createState() => _NewMessageState();
+}
+
+class _NewMessageState extends State<NewMessage> {
+  String _enteredMessage = '';
+
+  void _send(){
+    FocusScope.of(context).unfocus();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(top: 8),
+      padding: const EdgeInsets.all(8),
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              decoration: const InputDecoration(labelText: 'send..'),
+              onChanged: (value) {
+                setState(() {
+                  _enteredMessage = value;
+                });
+              },
+            ),
+          ),
+          IconButton(
+            onPressed: _enteredMessage.trim().isEmpty ? null : _send,
+            icon: const Icon(Icons.send),
+            color: Theme.of(context).colorScheme.primary,
+          )
+        ],
+      ),
+    );
+  }
+}
